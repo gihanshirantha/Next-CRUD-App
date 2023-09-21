0b8c7c93a44a5f3e0241454415799c741ed811a2
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-21 09:57:16 +0530

    Get data from form

--- src/Components/AddTask.tsx
+++ src/Components/AddTask.tsx
@@ -2,16 +2,37 @@
 
 import {BsFillPersonPlusFill} from 'react-icons/bs'
 import Modal from './Modal'
-import { useState } from 'react'
+import { FormEventHandler, useState } from 'react'
 
 const AddTask = () => {
 
 const [modalOpen,setModalOpen]=useState<boolean>(false);
+const [newEmpValue,setNewEmpValue]=useState<string>('')
+const [name, setName] = useState<string>("");
+const [roll,setRoll]=useState<string>('');
+const [salary,setSalary]=useState<string>('');
+
+
+const handleSubmitNewEmployee:FormEventHandler<HTMLFormElement>=(e)=>{
+    e.preventDefault();
+    console.log(name,roll,salary);
+}
 
   return (
     <div>
       <button onClick={()=>setModalOpen(true)} className="items-center bg-blue-500 py-2 w-full rounded-md text-white hover:bg-blue-700 transition flex justify-center">Add new Employee <BsFillPersonPlusFill className="ml-3" size={15}/></button>
-        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+            
+            <form action="submit" onSubmit={handleSubmitNewEmployee}>
+                <h2 className='font-bold text-lg text-center'>Add new Emplooyee</h2>
+                <div className="modal-action flex-col items-center">
+                <input  id='name'  onChange={e=>setName(e.target.value)} type="text" placeholder="Enter Name" className="input input-bordered w-full " />  
+                 <input id='roll'  onChange={e=>setRoll(e.target.value)} type="text" placeholder="Enter Your Job Roll" className="input input-bordered w-full mt-5 " />
+                <input id='salary'  onChange={e=>setSalary(e.target.value)} type="text" placeholder="Enter Salary" className="input input-bordered w-full mt-5 " /> 
+                <button type='submit' className="btn btn-wide mt-5  bg-blue-500  text-white hover:bg-blue-700 transition">Submit</button>
+                </div>
+            </form>
+        </Modal>
     </div>
   )
 }
--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,18 +1,19 @@
+import { Chilanka } from "next/font/google";
 
 
 interface ModalProps{
     modalOpen:boolean;
-    setModalOpen:(open:boolean)=>void;
+    setModalOpen:(open:boolean)=>boolean|void;
+    children:React.ReactNode;
 }
 
-const Modal:React.FC<ModalProps> = ({modalOpen,setModalOpen}) => {
+const Modal:React.FC<ModalProps> = ({modalOpen,setModalOpen,children}) => {
   return (
     <div>
         <div className={`modal ${modalOpen?"modal-open":""}`}>
         <div className="modal-box relative">
         <button onClick={()=> setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-        <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        {children}
         </div>
     </div>
     </div>
